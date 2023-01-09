3eecd624b13c54a61fb9dc5f5defe03bc96b27f4
digitsensitive <digit.sensitivee@gmail.com>
2023-01-09 22:08:54 +0100

    add draw border to snake

--- src/games/snake2.asm
+++ src/games/snake2.asm
@@ -24,6 +24,9 @@ init    ; Set border and background color to black
 
         jsr cls                 ; clear screen
 
+        ; draw border
+        jsr dbrd
+
         ; set current direction to right
         lda #$01
         sta dir
@@ -31,10 +34,10 @@ init    ; Set border and background color to black
         sta dir+1
 
         ; set snake start position
-        lda #13
-        sta snake
-        lda #08
-        sta snake+1
+        lda #01
+        sta s_pos
+        lda #01
+        sta s_pos+1
 
         ; TODO: add_new_food
         
@@ -57,7 +60,7 @@ delay   cmp $d012
         adc #$01
         sta time
 
-        jsr update
+        ; jsr update
         jsr draw
         ; --------------
 
@@ -71,8 +74,6 @@ delay   cmp $d012
 
 input   lda dir
         sta tmpdir
-        lda dir+1
-        sta tmpdir+1
 
         jsr $ff9f ; 65439, jump to scan keyboard
         jsr $ffe4 ; 65508, jump to get a character
@@ -96,41 +97,65 @@ input   lda dir
         rts
 
 uk      ; set current direction to up
-        lda #$00
+        lda #$01
         sta dir
-        lda #$02
-        sta dir+1
         rts
 
 dk      ; set current direction to down
-        lda #$00
+        lda #$03
         sta dir
-        lda #$01
-        sta dir+1
         rts
 
 lk      ; set current direction to left
-        lda #$02
-        sta dir
         lda #$00
-        sta dir+1
+        sta dir
         rts
 
 rk      ; set current direction to right
-        lda #$01
+        lda #$02
         sta dir
-        lda #$00
-        sta dir+1
         rts
 
 ; ----------------------------------------------------------
 ; Update
 ; ----------------------------------------------------------
 
-update  clc
-        lda snake
-        adc #1
-        sta snake
+update  lda dir
+
+        clc
+
+        cmp #0 ; left
+        beq left
+        cmp #1 ; up
+        beq up
+        cmp #2 ; right
+        beq right
+        cmp #3 ; down
+        beq down
+        rts
+
+left    lda #$01 ; TODO: FIX TO -1
+        sta s_pos
+        lda #$00
+        sta s_pos+1
+        rts
+
+up      lda #$00
+        sta s_pos
+        lda #$01 ; TODO: FIX TO -1
+        sta s_pos+1
+        rts
+
+right   lda #$01
+        sta s_pos
+        lda #$00
+        sta s_pos+1
+        rts
+
+down    lda #$00
+        sta s_pos
+        lda #$01
+        sta s_pos+1
         rts
 
 ; ----------------------------------------------------------
@@ -164,6 +189,48 @@ cls     lda #$20                ; spacebar screen code
                                 ; if no -> continue loop
         rts
 
+; Draw border
+; First draw top and bottom line of the border (brdtb)
+; Second draw left and right border (brdlr)
+
+dbrd    ldy #$00
+dbrdtb  ldx brdtb,y             ; get byte position
+        lda #102                ; load char
+        sta $0428,x             ; plot char to byte position: $0428+offset
+        sta $07c0,x             ; plot char to byte position: $07c0+offset
+        iny
+        cpy #$28                ; if equal zero flag is set to one
+        bne dbrdtb              ; branch if zero flag is zero
+        ldy #$00
+dbrdlr  ldx brdlr,y             ; get byte position
+        lda #102                ; load char
+        sta $0450,x             ; plot char to byte position: $0450+offset
+        sta $0478,x             ; ...
+        sta $04a0,x
+        sta $04c8,x
+        sta $04f0,x
+        sta $0518,x
+        sta $0540,x
+        sta $0568,x
+        sta $0590,x
+        sta $05b8,x
+        sta $05e0,x
+        sta $0608,x
+        sta $0630,x
+        sta $0658,x
+        sta $0680,x
+        sta $06a8,x
+        sta $06d0,x
+        sta $06f8,x
+        sta $0720,x
+        sta $0748,x
+        sta $0770,x
+        sta $0798,x
+        iny
+        cpy #$02                ; if equal zero flag is set to one
+        bne dbrdlr              ; branch if zero flag is zero
+        rts
+
 ; ----------------------------------------------------------
 ; Update Score
 ; ----------------------------------------------------------
@@ -187,11 +254,13 @@ uscore  sed ; set decimal mode
 ; Draw Snake
 ; ----------------------------------------------------------
 
-dsnake  ldx snake
+ ; x + (y * 40)
+dsnake  clc
+        lda s_pos ; load x pos
+        adc #80
+        sta s_pos+2
+        ldx s_pos+2
         lda #$80
-        sta $0400,x
-        dex
-        lda #$20                ; spacebar screen code
         sta $0400,x
         rts
 
@@ -232,17 +301,31 @@ plotd   clc
 ; ----------------------------------------------------------
 
 ; Current direction
-; Location of bytes from low to high: dir,dir+1
-; dir   => x value
-; dir+1 => y value
-dir     .byte 0,0
-tmpdir  .byte 0,0
+; Left  = 0 (x = -1, y = 0)
+; Up    = 1 (x = 0, y = -1)
+; Right = 2 (x = 1, y = 0)
+; Down  = 3 (x = 0, y = 1)
+dir     .byte 0
+tmpdir  .byte 0
 
 time    .byte 0,0
 
 snake   .byte 0,0
 
+; snake (x,y)
+s_pos   .byte 0,0,0
+s_lp    .byte 0,0
+
 ; Using Binary Coded Decimal (BCD)
 ; Location of bytes from low to high: score,score+1,score+2
 ; Each location stores a two-digit value
 score   .byte 0,0,0
+
+; Border data
+
+brdtb   .byte $00,$01,$02,$03,$04,$05,$06,$07,$08,$09
+        .byte $0a,$0b,$0c,$0d,$0e,$0f,$10,$11,$12,$13
+        .byte $14,$15,$16,$17,$18,$19,$1a,$1b,$1c,$1d
+        .byte $1e,$1f,$20,$21,$22,$23,$24,$25,$26,$27
+
+brdlr   .byte $00,$27
